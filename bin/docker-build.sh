b58487a9db0a33092fd93d465414f9f861b8ce38
GregLeBarbar <gregory.charmier.epfl.ch>
2017-03-14 14:14:45 +0100

    Refactoring views with APIView

--- bin/docker-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ../docker/django/requirements
-cp -r ../requirements ../docker/django
-cd ..
-docker-compose build
-
--- src/api/urls.py
+++ src/api/urls.py
@@ -1,14 +1,8 @@
 """(c) All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017"""
 
 from django.conf.urls import url
-from api import views, rancher, apikeyhandler
-import auth
+from api import views
 
-rancher_api = rancher.Rancher()
-apikey_handler = apikeyhandler.ApiKeyHandler()
-authenticator = auth.get_configured_authenticator()
-
-ammapp = views.AMMApp(authenticator, apikey_handler, rancher_api)
 
 urlpatterns = [
     url(r'^apikeys/$', views.KeysView.as_view(), name="apikeys"),
