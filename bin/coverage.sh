d4be6370606c581d4de22224dbde86a08cf16fe4
Laurent Boatto <me@me>
2017-05-02 09:36:20 +0200

    - reset password functionality

--- bin/coverage.sh
+++ bin/coverage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
+# coverage run --source='.' src/manage.py test --settings=config.settings.local api.tests.test_views.ViewsTestCase.test_reset_password
 coverage run --source='.' src/manage.py test --settings=config.settings.local
 coverage html
